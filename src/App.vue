c047a70c50aa956eb0636738af3affb4b324cd9c
xing-6464 <xing_6464@foxmail.com>
2022-09-09 20:49:23 +0800

    add validateEmail

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,86 @@
 <template>
   <div class='container'>
     <global-header :user="user"></global-header>
-    <column-list :list='list'></column-list>
+    <form action="">
+      <div class="mb-3">
+        <label class="form-label">邮箱地址</label>
+        <input
+          type="email" class="form-control"
+          v-model="emailRef.val"
+          @blur="validateEmail"
+        >
+        <div class="form-text" v-if="emailRef.error">{{ emailRef.message }}</div>
+      </div>
+      <div class="mb-3">
+        <label class="form-label">密码</label>
+        <input type="password" class="form-control">
+      </div>
+    </form>
   </div>
 </template>
 
 <script lang='ts'>
-import { defineComponent } from 'vue'
+import { defineComponent, reactive } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import ColumnList, { ColumnProps } from './components/ColumnList.vue'
 import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
 
 const testUser: UserProps = {
-  isLogin: true,
+  isLogin: false,
   name: 'xing',
   id: '1'
 }
 
-const testData: ColumnProps[] = [
-  {
-    id: 1,
-    title: 'test1的专栏',
-    description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 2,
-    title: 'test2的专栏',
-    description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧'
-    // avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 3,
-    title: 'test3的专栏',
-    description: '这是的test3专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  },
-  {
-    id: 4,
-    title: 'test4的专栏',
-    description: '这是的test4专栏，有一段非常有意思的简介，可以更新一下欧',
-    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-  }
-]
-
+// const testData: ColumnProps[] = [
+//   {
+//     id: 1,
+//     title: 'test1的专栏',
+//     description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
+//     avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+//   },
+//   {
+//     id: 2,
+//     title: 'test2的专栏',
+//     description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧'
+//     // avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+//   },
+//   {
+//     id: 3,
+//     title: 'test3的专栏',
+//     description: '这是的test3专栏，有一段非常有意思的简介，可以更新一下欧',
+//     avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+//   },
+//   {
+//     id: 4,
+//     title: 'test4的专栏',
+//     description: '这是的test4专栏，有一段非常有意思的简介，可以更新一下欧',
+//     avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+//   }
+// ]
+const emailReg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 export default defineComponent({
   name: 'App',
   components: {
-    ColumnList,
     GlobalHeader
   },
   setup () {
+    const emailRef = reactive({
+      val: '',
+      error: false,
+      message: ''
+    })
+    const validateEmail = () => {
+      if (emailRef.val.trim() === '') {
+        emailRef.error = true
+        emailRef.message = 'can not be empty'
+      } else if (!emailReg.test(emailRef.val)) {
+        emailRef.error = true
+        emailRef.message = 'should be valid email'
+      }
+    }
     return {
-      list: testData,
-      user: testUser
+      user: testUser,
+      emailRef,
+      validateEmail
     }
   }
 })
