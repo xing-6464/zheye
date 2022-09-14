53e70f42b0df18685f678566ae308f90af3fcd6a
xing-6464 <xing_6464@foxmail.com>
2022-09-14 17:23:51 +0800

    add Home.vue and Login.vue

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,19 @@
 <template>
   <div class='container'>
     <global-header :user="user"></global-header>
-    <validate-form @form-submit="onFormSubmit">
-      <div class="mb-3">
-        <label class="form-label">邮箱地址</label>
-        <validate-Input
-         :rules="emailRules"
-         v-model="emailVal"
-         placeholder="请输入邮箱地址"
-         type="text"
-        />
-      </div>
-      <div class="mb-3">
-        <label class="form-label">密码</label>
-        <validate-Input
-         :rules="passwordRules"
-         v-model="passwordVal"
-         placeholder="请输入密码"
-         type="password"
-        />
-      </div>
-      <template #submit>
-        <span class="btn btn-primary">Submit</span>
-      </template>
-    </validate-form>
+    <home></home>
+    <!-- <login></login> -->
   </div>
 </template>
 
 <script lang='ts'>
-import { defineComponent, ref } from 'vue'
+import { defineComponent } from 'vue'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
+
 import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
-import ValidateInput, { RulesProp } from './components/ValidateInput.vue'
-import ValidateForm from './components/ValidateForm.vue'
+import Home from './views/Home.vue'
+import Login from './views/Login.vue'
 
 const testUser: UserProps = {
   isLogin: false,
@@ -40,60 +21,16 @@ const testUser: UserProps = {
   id: '1'
 }
 
-// const testData: ColumnProps[] = [
-//   {
-//     id: 1,
-//     title: 'test1的专栏',
-//     description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
-//     avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-//   },
-//   {
-//     id: 2,
-//     title: 'test2的专栏',
-//     description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧'
-//     // avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-//   },
-//   {
-//     id: 3,
-//     title: 'test3的专栏',
-//     description: '这是的test3专栏，有一段非常有意思的简介，可以更新一下欧',
-//     avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-//   },
-//   {
-//     id: 4,
-//     title: 'test4的专栏',
-//     description: '这是的test4专栏，有一段非常有意思的简介，可以更新一下欧',
-//     avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-//   }
-// ]
 export default defineComponent({
   name: 'App',
   components: {
     GlobalHeader,
-    ValidateInput,
-    ValidateForm
+    Home
+    // Login
   },
   setup () {
-    const emailVal = ref('')
-    const emailRules: RulesProp = [
-      { type: 'required', message: '电子邮箱地址不能为空' },
-      { type: 'email', message: '请输入正确的电子邮箱地址' }
-    ]
-    const passwordVal = ref('')
-    const passwordRules: RulesProp = [
-      { type: 'required', message: '密码不能为空' },
-      { type: 'range', message: '密码必须不少于6位，不能大于16位，不能有空格', min: 6, max: 16 }
-    ]
-    const onFormSubmit = (result: boolean) => {
-      console.log('result: ', result)
-    }
     return {
-      user: testUser,
-      emailRules,
-      emailVal,
-      passwordVal,
-      passwordRules,
-      onFormSubmit
+      user: testUser
     }
   }
 })
---
+++ src/views/Login.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="login-page">
+    <validate-form @form-submit="onFormSubmit">
+      <div class="mb-3">
+        <label class="form-label">邮箱地址</label>
+        <validate-input
+          :rules="emailRules" v-model="emailVal"
+          placeholder="请输入邮箱地址"
+          type="text"
+          ref="inputRef"
+        />
+      </div>
+      <div class="mb-3">
+        <label class="form-label">密码</label>
+        <validate-input
+          type="password"
+          placeholder="请输入密码"
+          :rules="passwordRules"
+          v-model="passwordVal"
+        />
+      </div>
+    </validate-form>
+  </div>
+</template>
+
+<script lang='ts'>
+import { defineComponent, ref } from 'vue'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import ValidateInput, { RulesProp } from '../components/ValidateInput.vue'
+import ValidateForm from '../components/ValidateForm.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    ValidateInput,
+    ValidateForm
+  },
+  setup () {
+    const emailVal = ref('')
+    const emailRules: RulesProp = [
+      { type: 'required', message: '电子邮箱地址不能为空' },
+      { type: 'email', message: '请输入正确的电子邮箱地址' }
+    ]
+    const passwordVal = ref('')
+    const passwordRules: RulesProp = [
+      { type: 'required', message: '密码不能为空' },
+      { type: 'range', message: '密码必须不少于6位，不能大于16位，不能有空格', min: 6, max: 16 }
+    ]
+    const onFormSubmit = (result: boolean) => {
+      console.log('result: ', result)
+    }
+    return {
+      emailRules,
+      emailVal,
+      passwordVal,
+      passwordRules,
+      onFormSubmit
+    }
+  }
+})
+</script>
