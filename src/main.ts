85e762785308136ed068372e7a8ef3d3cb757b0c
xing-6464 <xing_6464@foxmail.com>
2022-09-15 13:49:42 +0800

    add vuex

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
+import { createStore } from 'vuex'
 
 import App from './App.vue'
 import Home from './views/Home.vue'
 import Login from './views/Login.vue'
 import ColumnDetail from './views/ColumnDetail.vue'
+
+const store = createStore({
+  state: {
+    conut: 0
+  },
+  mutations: {
+    add (state) {
+      state.conut++
+    }
+  }
+})
 
 const routerHistory = createWebHistory()
 const router = createRouter({
