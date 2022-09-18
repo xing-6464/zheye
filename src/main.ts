64aceef5bd6ea04923f1c6863caa9b5052b321dd
xing-6464 <xing_6464@foxmail.com>
2022-09-18 16:46:41 +0800

    参数后端接口

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { createApp } from 'vue'
 
+import axios from 'axios'
 import store from './store'
 import router from './router'
 
 import App from './App.vue'
 
+axios.defaults.baseURL = 'http://apis.imooc.com/api'
+axios.interceptors.request.use(config => {
+  config.params = { ...config.params, icode: 'A2D7D59EB2DF04B2' }
+  return config
+})
+axios.get('/columns', { params: { key: 'hello' } }).then(resp => {
+  console.log(resp.data)
+})
 const app = createApp(App)
 app.use(router)
 app.use(store)
